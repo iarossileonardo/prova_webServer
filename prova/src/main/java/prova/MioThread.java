4f1844913747eac2d6a23f90936be8cb8085964a
lai2-24 <lai2-24@itismeucci.com>
2024-12-11 09:00:34 +0100

    codice modificato

--- prova/src/main/java/prova/MioThread.java
+++ prova/src/main/java/prova/MioThread.java
@@ -5,39 +5,49 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class MioThread extends Thread{
+public class MioThread extends Thread {
     Socket s0;
 
-    public MioThread(Socket s0){
+    public MioThread(Socket s0) {
         this.s0 = s0;
     }
 
     @Override
-    public void run(){
+    public void run() {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream()));
             DataOutputStream out = new DataOutputStream(s0.getOutputStream());
-            int conta = 0;
-            String op = "";
-            
+            String firstLine = "";
+
+            firstLine = in.readLine();
+            String op = firstLine.split(" ")[0];
+            String resource = firstLine.split(" ")[1];
+            String version = firstLine.split(" ")[2];
+
+            String header;
             do {
-                String s1 = in.readLine();
-                if (conta == 0) {
-                    op = s1.split(" ")[0];
-                }
-                if ((s1.isEmpty()) && (op.equals("GET"))) {
-                    String bodyRisposta = "<html><body>Pagina trovata <br><br> EVVIVA</body></html>";
-                    out.writeBytes("HTTP/1.1 200 OK\r\n");
-                    out.writeBytes("Content-Length: " + bodyRisposta.length() + "\r\n");
-                    out.writeBytes("Content-Type: text/html\r\n");
-                    out.writeBytes("\r\n");
-                    out.writeBytes(bodyRisposta);
-                    break;
-                }
-                conta++;
-            } while (true);
+                header = in.readLine();
+                System.out.println(header);
+            } while (!header.isEmpty());
+
+            if ((resource.equals("/")) || (resource.equals("/index.html"))) {
+                String bodyRisposta = "<html><body>Pagina trovata <br><br><b>EVVIVA</b></body></html>";
+                out.writeBytes("HTTP/1.1 200 OK\r\n");
+                out.writeBytes("Content-Length: " + bodyRisposta.length() + "\r\n");
+                out.writeBytes("Content-Type: text/html\r\n");
+                out.writeBytes("\r\n");
+                out.writeBytes(bodyRisposta);
+            } else {
+                String bodyRisposta = "<html><body>Sei <b>fuori strada</b></body></html>";
+                out.writeBytes("HTTP/1.1 404 Not found\n");
+                out.writeBytes("Content-Lenght: " + bodyRisposta.length() + "\n");
+                out.writeBytes("Content-Type: text/html\n");
+                out.writeBytes("\n");
+                out.writeBytes(bodyRisposta);
+            }
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println("ERRORE");
+            e.printStackTrace();
         }
     }
 }
